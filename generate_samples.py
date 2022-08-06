a5f73ecbec9a25ea993e87b80fd8191bfcc8466d
TwistedAlex <muzihuole@gmail.com>
2022-08-06 11:47:18 -0500

    add sampling script

--- generate_samples.py
+++ generate_samples.py
@@ -22,8 +22,6 @@ count = 0
 for idx in range(1000):
     img = model.sample(1, device=device, T=20, T_latent=200)
     print(img.shape)
-    PIL.Image.fromarray(img[0].cpu().permute([1, 2, 0]).numpy(), 'RGB').save(output_path + f'{str(count).zfill(5)}' + '.png')
-    PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(
-        output_path + f'{str(count +1).zfill(5)}' + '.png')
+    PIL.Image.fromarray(img[0].cpu().permute([1, 2, 0]).numpy() * 255, 'RGB').save(output_path + f'{str(count).zfill(5)}' + '.png')
     exit(1)
     count = count + 1
