b9ace863a93f675d3e82140d389e7cf4f0da99b5
TwistedAlex <muzihuole@gmail.com>
2022-08-06 12:08:33 -0500

    add sampling script

--- generate_samples.py
+++ generate_samples.py
@@ -20,9 +20,8 @@ output_path = "outputs/"
 pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
 count = 0
 for idx in range(1000):
-    img = model.sample(1, device=device, T=20, T_latent=200)
+    img = model.sample(8, device=device, T=20, T_latent=200)
     print(img.shape)
-    PIL.Image.fromarray(img[0].permute([1, 2, 0]).cpu().detach().numpy().astype(
-        np.uint8), 'RGB').save(output_path + f'{str(count).zfill(5)}' + '.png')
+    PIL.Image.fromarray(img[0].permute([1, 2, 0]).cpu().detach().numpy(), 'RGB').save(output_path + f'{str(count).zfill(5)}' + '.png')
     exit(1)
     count = count + 1
